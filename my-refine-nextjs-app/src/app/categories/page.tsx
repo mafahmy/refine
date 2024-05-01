3b1cb13d25bc9db266684d54a9201253b1cffc23
Mahmoud Fahmy <mafahmypc@hotmail.com>
2024-05-01 14:16:26 +0300

    fixing typescript errors

--- my-refine-nextjs-app/src/app/categories/page.tsx
+++ my-refine-nextjs-app/src/app/categories/page.tsx
@@ -17,17 +17,17 @@ export default function CategoryList() {
 
   return (
     <List>
-      <Table {...tableProps} rowKey="id">
-        <Table.Column dataIndex="id" title={"ID"} />
-        <Table.Column dataIndex="title" title={"title"} />
+      <Table {...tableProps} rowKey='id'>
+        <Table.Column dataIndex='id' title={'ID'} />
+        <Table.Column dataIndex='title' title={'title'} />
         <Table.Column
-          title={"Actions"}
-          dataIndex="actions"
-          render={(_, record: BaseRecord) => (
+          title={'Actions'}
+          dataIndex='actions'
+          render={(_, record: typeof BaseRecord) => (
             <Space>
-              <EditButton hideText size="small" recordItemId={record.id} />
-              <ShowButton hideText size="small" recordItemId={record.id} />
-              <DeleteButton hideText size="small" recordItemId={record.id} />
+              <EditButton hideText size='small' recordItemId={record.id} />
+              <ShowButton hideText size='small' recordItemId={record.id} />
+              <DeleteButton hideText size='small' recordItemId={record.id} />
             </Space>
           )}
         />
--- my-refine-nextjs-app/src/components/auth-page/index.tsx
+++ my-refine-nextjs-app/src/components/auth-page/index.tsx
@@ -2,14 +2,14 @@
 import { AuthPage as AuthPageBase } from "@refinedev/antd";
 import type { AuthPageProps } from "@refinedev/core";
 
-export const AuthPage = (props: AuthPageProps) => {
+export const AuthPage = (props: typeof AuthPageProps) => {
   return (
     <AuthPageBase
       {...props}
       formProps={{
         initialValues: {
-          email: "info@refine.dev",
-          password: "refine-supabase",
+          email: 'info@refine.dev',
+          password: 'refine-supabase',
         },
       }}
     />
